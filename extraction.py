2a53461db65b8958d26f8c1f83d0786fcbc385b3
joerovar <joseph.rodriguez.v@gmail.com>
2022-02-20 17:24:26 -0500

    add avl travel times

--- extraction.py
+++ extraction.py
@@ -1,32 +1,36 @@
-# df3 = pd.read_csv('bus_avl_2019-09-03.csv')
-# df4 = pd.read_csv('bus_avl_2019-09-04.csv')
-# df5 = pd.read_csv('bus_avl_2019-09-05.csv')
-# df6 = pd.read_csv('bus_avl_2019-09-06.csv')
-#
+# df1 = pd.read_csv('in/raw/bus_avl_2019-09-10.csv')
+# df2 = pd.read_csv('in/raw/bus_avl_2019-09-11.csv')
+# df3 = pd.read_csv('in/raw/bus_avl_2019-09-12.csv')
+# df4 = pd.read_csv('in/raw/bus_avl_2019-09-13.csv')
+# date1 = '2019-09-10 00:00:00'
+# date2 = '2019-09-11 00:00:00'
+# date3 = '2019-09-12 00:00:00'
+# date4 = '2019-09-13 00:00:00'
+# df_sched = pd.read_csv('in/raw/route20_stop_time.csv')
+# df_sched = df_sched[['trip_id', 'stop_sequence', 'schd_sec', 'stop_id']]
 # df_main = pd.DataFrame()
 #
-# base_date = '2019-09-03 00:00:00'
-# for df in (df3, df4, df5, df6):
+# for (df, bdate) in ((df1, date1), (df2, date2), (df3,date3), (df4, date4)):
 #     d = df[df['route_id'] == str(20)]
-#     d = d[['route_id', 'trip_id', 'stop_sequence', 'event_time', 'departure_time',
-#            'dwell_time', 'passenger_load']]
-#     d['base_date'] = [base_date] * len(d.index)
-#     d['avl_sec'] = d['departure_time'].astype('datetime64[ns]') - d['base_date'].astype('datetime64[ns]')
-#     d['avl_sec'] = d['avl_sec'].dt.total_seconds()
-#     d = d.dropna(subset=['avl_sec'])
-#     d['avl_sec'] = d['avl_sec'].round(decimals=1)
+#     d = d[['route_id', 'trip_id', 'stop_sequence', 'event_time', 'departure_time']]
+#     d['trip_id'] = d['trip_id'].replace('', np.nan)
+#     d = d.dropna(subset=['trip_id'])
+#     d = d.astype({'trip_id': 'int64'})
+#     d = d.rename(columns={'event_time': 'avl_arr_time', 'departure_time': 'avl_dep_time'})
+#
+#     d['base_date'] = [bdate] * len(d.index)
+#
+#     d['avl_arr_sec'] = d['avl_arr_time'].astype('datetime64[ns]') - d['base_date'].astype('datetime64[ns]')
+#     d['avl_arr_sec'] = d['avl_arr_sec'].dt.total_seconds()
+#     d['avl_arr_sec'] = d['avl_arr_sec'].round(decimals=1)
+#
+#     d['avl_dep_sec'] = d['avl_dep_time'].astype('datetime64[ns]') - d['base_date'].astype('datetime64[ns]')
+#     d['avl_dep_sec'] = d['avl_dep_sec'].dt.total_seconds()
+#     d['avl_dep_sec'] = d['avl_dep_sec'].round(decimals=1)
+#
 #     d = d.drop('base_date', axis=1)
+#
+#     d = d.merge(df_sched, left_on=['trip_id', 'stop_sequence'], right_on=['trip_id', 'stop_sequence'])
 #     df_main = df_main.append(d, ignore_index=True)
-
-# df_main['trip_id'] = df_main['trip_id'].replace('', np.nan)
-# df_main = df_main.dropna(subset=['trip_id'])
-# df_main['trip_id'] = df_main['trip_id'].astype(str).str[:-2]
-# df_main['trip_id'] = df_main['trip_id'].astype(int)
-# df_main['route_id'] = df_main['route_id'].astype(int)
-# df_main.to_csv('route20_stop_time_dwell.csv', index=False)
-# df_dwell = pd.read_csv('route20_stop_time_dwell.csv')
-# df_no_dwell = pd.read_csv('route20_stop_time.csv')
-# df_no_dwell = df_no_dwell[['schd_trip_id', 'stop_id', 'stop_sequence', 'arrival_time', 'schd_sec', 'event_time']]
-# df_main = df_dwell.merge(df_no_dwell, left_on=['trip_id', 'stop_sequence', 'event_time'],
-#                          right_on=['schd_trip_id', 'stop_sequence', 'event_time'])
-# df_main.to_csv('route20_stop_time_final.csv', index=False)
+# df_main = df_main.sort_values(by=['trip_id', 'stop_sequence'])
+# df_main.to_csv('in/raw/rt20_extra.csv', index=False)
